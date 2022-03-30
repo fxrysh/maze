52335616a2e410c639d1ee16e000b789cf738fdd
kyuzu <99307966+kyuzu@users.noreply.github.com>
2022-03-30 20:01:15 +0800

    feat: project reset

--- mod.ts
+++
@@ -1,6 +0,0 @@
-import { dirname, resolve } from 'https://deno.land/std@0.132.0/path/mod.ts';
-
-if (Deno.env.get('OS') === 'Windows_NT') {
-    const DENO_SDL2_PATH = resolve(dirname(Deno.execPath()), 'lib\\SDL2');
-    Deno.env.set('DENO_SDL2_PATH', DENO_SDL2_PATH);
-}
